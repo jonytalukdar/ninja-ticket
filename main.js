59b0433c37e0d5376139cf62551f215de863b0a5
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-29 23:43:20 +0600

    created a function to reduce code

--- main.js
+++ main.js
@@ -1,25 +1,16 @@
-document
-  .getElementById('economy-increase')
-  .addEventListener('click', function () {
-    const economyInput = document.getElementById('economy-count');
-    const economyCount = parseInt(economyInput.value);
-    const economyNewCount = economyCount + 1;
-    economyInput.value = economyNewCount;
+function handleProductChange(isIncrease) {
+  const economyInput = document.getElementById('economy-count');
+  const economyCount = parseInt(economyInput.value);
+  //   const economyNewCount = economyCount - 1;
+  let economyNewCount = 0;
+  if (isIncrease == true) {
+    economyNewCount = economyCount + 1;
+  }
+  if (isIncrease == false && economyCount > 0) {
+    economyNewCount = economyCount - 1;
+  }
+  economyInput.value = economyNewCount;
 
-    const economyTotalPrice = economyNewCount * 100;
-
-    document.getElementById('economy-total').innerText = economyTotalPrice;
-  });
-
-document
-  .getElementById('economy-decrease')
-  .addEventListener('click', function () {
-    const economyInput = document.getElementById('economy-count');
-    const economyCount = parseInt(economyInput.value);
-    const economyNewCount = economyCount - 1;
-    economyInput.value = economyNewCount;
-
-    const economyTotalPrice = economyNewCount * 100;
-
-    document.getElementById('economy-total').innerText = economyTotalPrice;
-  });
+  const economyTotalPrice = economyNewCount * 100;
+  document.getElementById('economy-total').innerText = economyTotalPrice;
+}
