25047deeb32bb4f3b5fab64e86ee5f847404e7ee
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-30 01:07:00 +0600

    completed calulate

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function handleProductChange(quality, increase) {
   const qualityInput = document.getElementById(quality + '-count');
-  const qualityCount = parseInt(qualityInput.value);
+  const qualityCount = getInput(quality);
   let qualityNewCount = 0;
   // condition for increase decrease
   if (increase == true) {
@@ -20,4 +20,23 @@ function handleProductChange(quality, increase) {
     qualityTotal = qualityNewCount * 100;
   }
   document.getElementById(quality + '-total').innerText = qualityTotal;
+
+  calculalateTotal();
+}
+
+function calculalateTotal() {
+  const firstClassCount = getInput('first-class');
+  const economyCount = getInput('economy');
+
+  //   const economyInput = document.getElementById('economy-count').value;
+  //   const economyCount = parseInt(economyInput);
+
+  const totalPrice = firstClassCount * 150 + economyCount * 100;
+  document.getElementById('sub-total').innerText = totalPrice;
+}
+
+function getInput(quality) {
+  const qualityInput = document.getElementById(quality + '-count').value;
+  const qualityCount = parseInt(qualityInput);
+  return qualityCount;
 }
