f0672de697987940a11cf16d22e8a6074d0a6c11
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-29 23:29:11 +0600

    completed decrease part

--- main.js
+++ main.js
@@ -8,6 +8,18 @@ document
 
     const economyTotalPrice = economyNewCount * 100;
 
-    // const economyTotal = inputNewNumber * 100;
+    document.getElementById('economy-total').innerText = economyTotalPrice;
+  });
+
+document
+  .getElementById('economy-decrease')
+  .addEventListener('click', function () {
+    const economyInput = document.getElementById('economy-count');
+    const economyCount = parseInt(economyInput.value);
+    const economyNewCount = economyCount - 1;
+    economyInput.value = economyNewCount;
+
+    const economyTotalPrice = economyNewCount * 100;
+
     document.getElementById('economy-total').innerText = economyTotalPrice;
   });
